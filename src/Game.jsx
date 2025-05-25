b2bc9f035d2a824e71390fde7f1d3d87f9d576ad
Nick536363 <134803463+Nick536363@users.noreply.github.com>
2025-05-25 17:33:33 +0200

    added using of function from App.js

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,15 +1,17 @@
 import './Game.css'
 
 function Game(props){
+
     return(
         <div className="Game">
         <div className="progress"></div>
         <h1>{props.question["title"]}</h1>
 
         <ul>
-            <li>{props.question["variants"][0]}</li>
-            <li>{props.question["variants"][1]}</li>
-            <li>{props.question["variants"][2]}</li>
+            <li onClick={() => {props.onClickVariant(0)}}>{props.question["variants"][0]}</li>
+            <li onClick={() => {props.onClickVariant(1)}}>{props.question["variants"][1]}</li>
+            <li onClick={() => {props.onClickVariant(2)}}>{props.question["variants"][2]}</li>
+
         </ul>
 
         </div>
