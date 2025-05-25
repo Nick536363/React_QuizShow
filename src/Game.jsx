183bba797cf4879bc76b1aa0179baeabb3d0cd40
Nick536363 <134803463+Nick536363@users.noreply.github.com>
2025-05-25 13:22:03 +0200

    added props from App.js + removed text from div of progress line

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,15 +1,15 @@
 import './Game.css'
 
-function Game(){
+function Game(props){
     return(
         <div className="Game">
         <div className="progress"></div>
-        <h1>Что такое useState?</h1>
+        <h1>{props.question["title"]}</h1>
 
         <ul>
-            <li>Это функция для хранения данных компонента.</li>
-            <li>Это глобальный стейт</li>
-            <li>Это когда включешь компьютер</li>
+            <li>{props.question["variants"][0]}</li>
+            <li>{props.question["variants"][1]}</li>
+            <li>{props.question["variants"][2]}</li>
         </ul>
 
         </div>
